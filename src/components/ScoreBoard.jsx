d547e8a9c0df364a2fbbe7f8c151815445afa999
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-03-05 08:10:32 +0300

    scoreboard component create

---
+++ src/components/ScoreBoard.jsx
@@ -0,0 +1,14 @@
+// this component keeps track of each player scores, it will useState to update the
+//  scores and also to keep track of the player turns so as to avoid updating the wrong player scores
+
+import React from "react";
+
+function ScoreBoard(){
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ScoreBoard;
